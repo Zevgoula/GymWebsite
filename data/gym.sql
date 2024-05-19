393018830f69ac7974f2b2d5179e54ac86cc4c98
George Papoutsas <osfpgeopap1925@gmail.com>
2024-05-20 01:03:40 +0300

    filled db with more data

--- data/gym.sql
+++ data/gym.sql
@@ -1,79 +1,71 @@
-CREATE TABLE IF NOT EXISTS "User" (
-	"username" string,
-	"password" string,
-	"role" string,
-	PRIMARY KEY ("username")
-);
+CREATE TABLE "User" (
+	"username"	string NOT NULL UNIQUE,
+	"password"	string NOT NULL,
+	"role"	TEXT NOT NULL,
+	PRIMARY KEY("username")
+)
 
-CREATE TABLE IF NOT EXISTS "Trainer" (
-	"trainer_id" integer,
-	"username" string,
-	"date_of_Birth" datetime,
-	"no_of_clients" integer,
-	"experience" integer,
-	"bookability" boolean,
-	"specialty" integer,
-	"fname" string,
-	"lname" string,
-	"email" string,
-	"phone_number" integer,
-	PRIMARY KEY ("trainer_id", "username"),
-	FOREIGN KEY ("username") REFERENCES "User" ("username")
-            ON UPDATE RESTRICT
-            ON DELETE RESTRICT
-);
+CREATE TABLE "Trainer" (
+	"trainer_id"	INTEGER NOT NULL,
+	"username"	string NOT NULL,
+	"date_of_Birth"	datetime,
+	"no_of_clients"	integer,
+	"experience"	integer,
+	"bookability"	boolean,
+	"specialty"	integer,
+	"fname"	string,
+	"lname"	string,
+	"email"	string,
+	"phone_number"	integer,
+	FOREIGN KEY("username") REFERENCES "User"("username") ON UPDATE RESTRICT ON DELETE RESTRICT,
+	PRIMARY KEY("trainer_id" AUTOINCREMENT)
+)
 
-CREATE TABLE IF NOT EXISTS "Customer" (
-	"customer_id" ,
-	"username" string,
-	"fname" string,
-	"lname" string,
-	"email" string,
-	"phone_number" integer,
-	"gender" string,
-	"height" float,
-	"weight" float,
-	"city" string,
-	"date_joined" datetime,
-	"date_of_birth" datetime,
-	PRIMARY KEY ("customer_id", "username"),
-	FOREIGN KEY ("username") REFERENCES "User" ("username")
-            ON UPDATE CASCADE
-            ON DELETE CASCADE
-);
+CREATE TABLE "Customer" (
+	"customer_id"	INTEGER NOT NULL,
+	"username"	string,
+	"fname"	string,
+	"lname"	string,
+	"email"	string,
+	"phone_number"	integer,
+	"gender"	string,
+	"height"	float,
+	"weight"	float,
+	"city"	string,
+	"date_joined"	datetime,
+	"date_of_birth"	datetime,
+	FOREIGN KEY("username") REFERENCES "User"("username") ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY("customer_id" AUTOINCREMENT)
+)
 
-CREATE TABLE IF NOT EXISTS "Gym" (
-	"gym_id" integer,
-	"phone_number" integer,
-	"city" string,
-	"street" string,
-	"stree_number" string,
-	PRIMARY KEY ("gym_id")
-);
+CREATE TABLE "Gym" (
+	"gym_id"	INTEGER NOT NULL,
+	"phone_number"	integer,
+	"city"	string,
+	"street"	string,
+	"stree_number"	string,
+	PRIMARY KEY("gym_id" AUTOINCREMENT)
+)
 
-CREATE TABLE IF NOT EXISTS "Anhkei" (
-	"username" string,
-	"gym_id" integer,
-	PRIMARY KEY ("username", "gym_id"),
-	FOREIGN KEY ("username") REFERENCES "User" ("username")
-            ON UPDATE CASCADE
-            ON DELETE CASCADE,
-	FOREIGN KEY ("gym_id") REFERENCES "Gym" ("gym_id")
-            ON UPDATE CASCADE
-            ON DELETE CASCADE
-);
+CREATE TABLE "Anhkei" (
+	"username"	string NOT NULL UNIQUE,
+	"gym_id"	integer NOT NULL,
+	FOREIGN KEY("username") REFERENCES "User"("username") ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY("gym_id") REFERENCES "Gym"("gym_id") ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY("username","gym_id")
+)
 
-CREATE TABLE IF NOT EXISTS "Membership" (
-	"membership_id" integer,
-	"type" string,
-	"montly_cost" float,
-	"description" string,
-	"date_created" datetime,
-	" " ,
-	PRIMARY KEY ("membership_id")
-);
+CREATE TABLE "Membership" (
+	"membership_id"	INTEGER NOT NULL,
+	"type"	string,
+	"montly_cost"	float,
+	"description"	string,
+	"date_created"	datetime,
+	" "	,
+	PRIMARY KEY("membership_id" AUTOINCREMENT)
+)
 
-CREATE TABLE IF NOT EXISTS "Includes" (
+CREATE TABLE "Includes" (
 	"gym_id" integer,
 	"membership_id" integer,
 	PRIMARY KEY ("gym_id", "membership_id"),
@@ -83,19 +75,20 @@ CREATE TABLE IF NOT EXISTS "Includes" (
 	FOREIGN KEY ("membership_id") REFERENCES "Membership" ("membership_id")
             ON UPDATE CASCADE
             ON DELETE CASCADE
-);
+)
 
-CREATE TABLE IF NOT EXISTS "Class" (
-	"class_id" integer,
-	"name" string,
-	"bookability" boolean,
-	"day" string,
-	"time" string,
-	"location" string,
-	PRIMARY KEY ("class_id")
-);
+CREATE TABLE "Class" (
+	"class_id"	INTEGER NOT NULL,
+	"name"	string,
+	"bookability"	boolean,
+	"day"	string,
+	"time"	string,
+	"location"	string,
+	"room_capacity"	INTEGER,
+	PRIMARY KEY("class_id" AUTOINCREMENT)
+)
 
-CREATE TABLE IF NOT EXISTS "Supervises" (
+CREATE TABLE "Supervises" (
 	"trainer_id" integer,
 	"class_id" integer,
 	PRIMARY KEY ("trainer_id", "class_id"),
@@ -105,9 +98,9 @@ CREATE TABLE IF NOT EXISTS "Supervises" (
 	FOREIGN KEY ("class_id") REFERENCES "Class" ("class_id")
             ON UPDATE CASCADE
             ON DELETE CASCADE
-);
+)
 
-CREATE TABLE IF NOT EXISTS "Offers" (
+CREATE TABLE "Offers" (
 	"membership_id" integer,
 	"class_id" integer,
 	PRIMARY KEY ("membership_id", "class_id"),
@@ -117,24 +110,20 @@ CREATE TABLE IF NOT EXISTS "Offers" (
 	FOREIGN KEY ("class_id") REFERENCES "Class" ("class_id")
             ON UPDATE CASCADE
             ON DELETE CASCADE
-);
+)
 
-CREATE TABLE IF NOT EXISTS "Books" (
-	"book_id" integer,
-	"day" string,
-	"time" string,
-	"class_id" integer,
-	"customer_id" integer,
-	PRIMARY KEY ("book_id", "class_id", "customer_id"),
-	FOREIGN KEY ("class_id") REFERENCES "Class" ("class_id")
-            ON UPDATE CASCADE
-            ON DELETE CASCADE,
-	FOREIGN KEY ("customer_id") REFERENCES "Customer" ("customer_id")
-            ON UPDATE CASCADE
-            ON DELETE CASCADE
-);
+CREATE TABLE "Books" (
+	"book_id"	integer NOT NULL,
+	"day"	string,
+	"time"	string,
+	"class_id"	integer NOT NULL,
+	"customer_id"	integer NOT NULL,
+	PRIMARY KEY("book_id","class_id","customer_id"),
+	FOREIGN KEY("customer_id") REFERENCES "Customer"("customer_id") ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY("class_id") REFERENCES "Class"("class_id") ON UPDATE CASCADE ON DELETE CASCADE
+)
 
-CREATE TABLE IF NOT EXISTS "Selects" (
+CREATE TABLE "Selects" (
 	"customer_id" integer,
 	"membership_id" integer,
 	PRIMARY KEY ("customer_id", "membership_id"),
@@ -144,5 +133,5 @@ CREATE TABLE IF NOT EXISTS "Selects" (
 	FOREIGN KEY ("membership_id") REFERENCES "Membership" ("membership_id")
             ON UPDATE CASCADE
             ON DELETE CASCADE
-);
+)
 
