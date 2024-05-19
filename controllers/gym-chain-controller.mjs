67bc633c437d5ecde3196d9b181e4da14d067895
Papiqulos <osfpgeopap1925@gmail.com>
2024-05-19 04:49:29 +0300

    cleaned up backend code

--- controllers/gym-chain-controller.mjs
+++ controllers/gym-chain-controller.mjs
@@ -10,26 +10,8 @@ if (process.env.NODE_ENV !== 'production') {
 /* Διαλέξτε το κατάλληλο μοντέλο στο αρχείο .env */
 const model = await import(`../model/gym-chain-model-${process.env.MODEL}.mjs`);
 
-export async function mainLayout(req, res, next) {
-   res.locals.header = 'header'; // Set the header template
-   res.locals.footer = 'footer'; // Set the footer template
-   next();
-}
-
-export async function createAccount(req, res) {
-    res.render('create-account', { layout: 'main' });
-}
-
-export async function home(req, res) {
-   res.render('home', { layout: 'main' });
-}
-
-export async function login(req, res) {
-    res.render('login', { layout: 'main' });
-}
-
-export async function memberships(req, res) {
-    res.render('memberships', { layout: 'main' });
+export async function about_classes(req, res) {
+    res.render('about_classes', { layout: 'main' });
 }
 
 export async function about_page(req, res) {
@@ -40,10 +22,34 @@ export async function services(req, res) {
     res.render('services', { layout: 'main' });
 }
 
+export async function home(req, res) {
+   res.render('home', { layout: 'main' });
+}
 
+export async function memberships(req, res) {
+    res.render('memberships', { layout: 'main' });
+}
 
+export async function personal_info(req, res) {
+    res.render('personal_info', { layout: 'main' });
+}
 
+export async function payment_info(req, res) {
+    res.render('payment_info', { layout: 'main' });
+}
 
+export async function login(req, res) {
+    res.render('login', { layout: 'main' });
+}
 
+export async function createAccount(req, res) {
+    res.render('create-account', { layout: 'main' });
+}
 
+export async function joinNow(req, res) {
+    res.render('joinNow', { layout: 'main' });
+}
 
+export async function contact(req, res) {
+    res.render('contact', { layout: 'main' });
+}
